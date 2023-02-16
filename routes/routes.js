aa9e7540738e0fb404271b3e2aec88ef078c73a0
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2023-02-16 13:24:42 -0600

    MongoDB

---
+++ routes/routes.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var userController = require('../src/user/userController');
+const router = express.Router();
+
+// ruta para login
+router.route('/user/login').post(userController.loginUserControllerFunc);
+// ruta para crear usuario
+router.route('/user/create').post(userController.createUserControllerFunc);
+//ruta para buscar usuario
+router.route('/user/search').post(userController.searchUserControllerFunc);
+
+module.exports = router;
